8fbc051dcf6b7b285474284e010c463554d8e0b1
Syed Naqi <naqishah2997@gmail.com>
2019-07-13 15:31:27 +0500

    Run the while loop

--- templates/template-custom.php
+++ templates/template-custom.php
@@ -6,6 +6,25 @@ get_header();
 <div class="container">
     <div class="row">
         <div class="content">
+            <h1>Test Custom Post Meta</h1>
+            <?php 
+                $custom_try = new WP_QUERY( array(
+                    'post_type' => 'custom_post',
+                    'posts_per_page' => -1,
+
+
+                ));
+
+                if($custom_try->have_posts()): while($custom_try->have_posts()): $custom_try->the_post();
+            ?>
+
+
+<!-- Template to Display Posts -->
+
+<!-- End here -->
+                    <?php 
+                    endwhile; endif;
+                    ?>
 
         </div>
     </div>
