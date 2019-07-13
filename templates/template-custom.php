05efa243fc7934b4afed917f2c8d91ea52bc0d6c
Syed Naqi <naqishah2997@gmail.com>
2019-07-13 15:26:10 +0500

    Added Styles and Container

--- templates/template-custom.php
+++ templates/template-custom.php
@@ -1,9 +1,18 @@
 <?php
 /* Template Name: Custom Page Template */
 get_header();
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="content">
+
+        </div>
+    </div>
+</div>
 
 
-
+<?php
 get_footer();
 
 ?>
